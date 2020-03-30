c90e73240884705d9c0144db055e2f08bf20c7a5
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 01:57:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,26 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0 
+def line(array)
+  if array.length == 0 
     puts "The line is currently empty."
   else 
   end 
 end 
 
-def take_a_number(katz_deli, name)
-  if katz_deli.length == 0
-    katz_deli << name
-  elsif katz_deli.length >= 1 
-    katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(array, name)
+  if array.length == 0
+    array << name
+  elsif array.length >= 1 
+    array << name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
   end
 end 
 
-def now_serving(katz_deli)
+def now_serving(array)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.length >= 1 
-    "Currently serving #{katz_deli[0]}"
+  elsif array.length >= 1 
+    "Currently serving #{array[0]}"
   end
-    katz_deli.shift()
+    array.shift()
 end 
