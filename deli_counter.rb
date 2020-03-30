4cf78afaefab0b0cbcff737cf5ff8d158425b35a
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 02:19:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,33 @@
-# Write your code here.
-
 katz_deli = []
 
-def line(array) 
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
+def line(array)
+  line = []
+  if array.length == 0 
     puts "The line is currently empty."
+  elsif array.length >=1 
+    array.collect.each_with_index do |name, index|
+      line << " #{index + 1}. #{name}"
+    end 
+      puts "The line is currently:#{line.join}"
+  end 
+end
+
+def take_a_number(array, name)
+  if array.length == 0
+    array << name
+    puts "Welcome, #{name}. You are number 1 in line."
+  elsif array.length >= 1 
+    array << name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
   end
-end
+end 
 
-
-def take_a_number(katz_deli, name)
-    katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0 
+def now_serving(array)
+  if array.length == 0 
     puts "There is nobody waiting to be served!"
-  else 
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+  elsif array.length >= 1 
+    remove = []
+    remove << array.shift
+    puts "Currently serving #{remove.join}."
   end
 end 
