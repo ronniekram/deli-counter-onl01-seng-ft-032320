aaa48ffbd2c96b57464d8854d113517ebf87cb74
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 02:11:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
 def line(array)
+  line = []
   if array.length == 0 
     puts "The line is currently empty."
   elsif array.length >=1 
-    line = []
     array.each_with_index |name, index|
       line << "#{index + 1}. #{name}"
       puts "The line is currently: #{line.join}"
