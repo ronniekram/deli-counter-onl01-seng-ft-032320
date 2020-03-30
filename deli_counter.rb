4bfde772a2c72dbed39518556fbd61f60335ce34
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 02:12:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   elsif array.length >=1 
-    array.each_with_index |name, index|
+    array.each_with_index do |name, index|
       line << "#{index + 1}. #{name}"
       puts "The line is currently: #{line.join}"
     end 
